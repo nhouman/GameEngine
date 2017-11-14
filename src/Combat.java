f63f3d75aa0759da4f00d8863e62dd459622fbca
Billy Bibbs <wbibbs@arcadia.edu>
2017-11-13 23:18:39 -0500

    Minor pushes

---
+++ src/Combat.java
@@ -0,0 +1,4 @@
+
+public class Combat {
+//Combat is going to work in a queue system. Attacks are queued against each other, and the actions taken affect the other party
+}
--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 		while (run)
 		{
-			System.out.println("Map printed");
+			System.out.println("Map printed");//Might be able to do with recursion
 			getMovement();
 			updateMap();
 		}
@@ -23,7 +23,6 @@ public class Main {
 	public static void startGame()
 	{
 		m.makeMap();
-		EntityGrid[3][3] = new Entity("Ass", 10);
 		EntityGrid[4][4] = new Entity("Item", 10);
 		m.updateMap(3, 3, 4);
 		m.updateMap(4, 4, 10);
