652eff640ceb51397a2f4f174f598f722b91b46b
Billy Bibbs <wbibbs@arcadia.edu>
2017-11-20 16:32:37 -0500

    Small tweaks to LinkedList, still working on it

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -3,9 +3,26 @@ public class LinkedList<E> {
 
 	private Node<E> curr;
 	private Node<E> next;
+	private int size = 0;
 	
 	public LinkedList(E a) {
 		curr.setHead(a);;
 		next = null;
+	}
+	
+	public LinkedList() {
+		curr = null;
+		next = null;
+	}
+
+	public void add(E a) {
+		size++;
+		curr.setNext((E) new Node());
+		curr.setHead(a);
+		//Is supposed to set the first item in the list to point to next via tail, then makes a new object in next
+		
+	}
+	public int size() {
+		return size;
 	}
 }
--- src/Node.java
+++ src/Node.java
@@ -2,37 +2,37 @@
 public class Node<E> {
 
 	/**
-	 * Declares a head and tail for the node to use
+	 * Declares a head and next for the node to use
 	 */
-	private E head, tail;
+	private E head, next;
 	
 	/**
-	 * Constructor setting head and tail to null
+	 * Constructor setting head and next to null
 	 */
 	public Node() {
 		head = null;
-		tail = null;
+		next = null;
 	}
 	
 	/**
-	 * Constructor setting head equal to a given value, and tail to null
+	 * Constructor setting head equal to a given value, and next to null
 	 * @param head Input value that sets current Node head to input value
 	 */
 	public Node(E head) {
 		this.head = head;
-		tail = null;
+		next = null;
 	}
 	
 	public E getHead() {
 		return head;
 	}
 	
-	public E getTail() {
-		return tail;
+	public E getNext() {
+		return next;
 	}
 	
-	public void setTail(E tail) {
-		this.tail = tail;
+	public void setNext(E next) {
+		this.next = next;
 	}
 	
 	public void setHead(E head) {
