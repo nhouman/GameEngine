37654589eb5cc19112874be24e4100007243caa7
Ricardo Rivera <rrivera_02@arcadia.edu>
2017-12-02 16:04:59 -0500

    added linkedlist

--- src/PartTwo/LinkedList.java
+++ src/PartTwo/LinkedList.java
@@ -1,5 +1,40 @@
-package PartTwo;
+package List;
 
-public class LinkedList {//Make for everything else: Queue, Inventory, Stack
+import List.Set;
+import List.node;
 
+class node<E> {
+	int item;
+    node<E> next;
+	private Set inputSet;
+	
+
+    @SuppressWarnings("null")
+	node() 
+	{
+      item = (Integer) null;
+      next = null;
+      
+    }
+
+    node(int item) {
+      this.item = item;
+      next = null;
+    }
+
+    node(int item, node<E> next) {
+      this.item = item;
+      this.next = next;
+    }
+
+    node(node<E> n) {
+      this.item = n.item;
+      this.next = n.next;
+    }
+
+	node(Set inputSet) 
+	{
+		this.inputSet = inputSet;
+		
+	}
 }
